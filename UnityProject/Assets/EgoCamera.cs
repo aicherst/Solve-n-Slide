ebe6ef6470043b4d3f869dd28f4ef7e1dcf49451
Stefan Aicher <stefanaicher1@gmx.de>
2017-05-10 20:48:32 +0200

    Add test files
    Add test asset folder for Stefan
    Add test scene and test terrain for Stefan
    Add EgoCamera and UIController scripts

---
+++ UnityProject/Assets/EgoCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EgoCamera : MonoBehaviour {
+    public float sensitivityX = 15F;
+    public float sensitivityY = 15F;
+
+    float rotationY = 0F;
+
+    void Update() {
+            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, -90, 90);
+
+            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+    }
+
+    void Start() {
+    }
+}
---
+++ UnityProject/Assets/UIController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+    public Text healthText;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void setHealth(int health) {
+        string healthString;
+        if (health < 10)
+            healthString = "00" + health;
+        else if (health < 100)
+            healthString = "0" + health;
+        else
+            healthString = health.ToString();
+        healthText.text = "Test Health: " + healthString + "/100";
+    }
+}
