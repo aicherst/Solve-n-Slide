b4728db6f5f960c85b7ba3bcc09ad1587db2e97f
Stefan Aicher <stefanaicher1@gmx.de>
2017-05-11 18:05:03 +0200

    Implement movement and UI
    implement IJetpack and ICharacterMovement interfaces
    implement update of UI

--- UnityProject/Assets/Character.cs
+++ UnityProject/Assets/Character.cs
@@ -3,18 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Character : MonoBehaviour {
-    public UIController uiController;
     public int health = 100, maxHealth = 100;
-    public int fuel = 100, maxFuel = 100;
     public int charges = 3, maxCharges = 5;
+    IJetpack jetpack;
+    ICharacterMovement characterMovement;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Awake() {
+        jetpack = GetComponent<IJetpack>();
+        characterMovement = GetComponent<ICharacterMovement>();
+    }
+
+    // Use this for initialization
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        jetpack.Thrust(Input.GetButton("Jetpack"));
+        Vector2 movementAxis = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        characterMovement.MovementInput(movementAxis);
+    }
+
+    public int getHealth() { return health; }
+    public int getMaxHealth() { return maxHealth; }
+
+    public float getFuel() { return jetpack.fuel; }
+    public float getMaxFuel() { return jetpack.maxFuel; }
+
+    public int getCharges() { return charges; }
+    public int getMaxCharges() { return maxCharges; }
 }
--- UnityProject/Assets/UIController.cs
+++ UnityProject/Assets/UIController.cs
@@ -7,38 +7,27 @@ public class UIController : MonoBehaviour {
     public Text healthText;
     public Text fuelText;
     public Text chargesText;
+    public Character character;
 
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	}
-
-    void setHealth(int health, int maxHealth) {
-        string healthString;
-        if (health < 10)
-            healthString = "00" + health;
-        else if (health < 100)
-            healthString = "0" + health;
-        else
-            healthString = health.ToString();
-        healthText.text = "Test Health: " + healthString + "/" + maxHealth;
+    // Use this for initialization
+    void Start() {
     }
 
-    void setFuel(int fuel, int maxFuel) {
-        string fuelString;
-        if (fuel < 10)
-            fuelString = "00" + fuel;
-        else if (fuel < 100)
-            fuelString = "0" + fuel;
-        else
-            fuelString = fuel.ToString();
-        healthText.text = "Test Fuel: " + fuelString + "/" + maxFuel;
+    // Update is called once per frame
+    void Update() {
+        healthText.text = "Test Health: " + makeThreeChars(character.getHealth()) + "/" + makeThreeChars(character.getMaxHealth());
+        fuelText.text = "Test Fuel: " + makeThreeChars(Mathf.RoundToInt(character.getFuel())) + "/" + makeThreeChars(Mathf.RoundToInt(character.getMaxFuel()));
+        chargesText.text = "Test Charges: " + character.getCharges() + "/" + character.getMaxCharges();
     }
 
-    void setCharges(int charges, int maxCharges) {
-        healthText.text = "Test Fuel: " + charges + "/" + maxCharges;
+    private string makeThreeChars(int value) {
+        string valueString;
+        if (value < 10)
+            valueString = "00" + value;
+        else if (value < 100)
+            valueString = "0" + value;
+        else
+            valueString = value.ToString();
+        return valueString;
     }
 }
