81f25652b78a568eb5aa965efb44112a66de8102
Markus <Markus.Ansorge@gmx.de>
2017-05-11 17:42:56 +0200

    Added character movement scripts
    - Jetpack
    - Character movement interface: ICharacterMovement
    - CharacterMovement

---
+++ UnityProject/Assets/CharacterMovement.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterMovement : MonoBehaviour, ICharacterMovement {
+    private CharacterController characterController;
+    private Vector3 velocity;
+
+    private bool wasGrounded;
+
+    private Vector2 input;
+
+    public GroudAttributes groudAttributes;
+
+    private void Awake() {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Use this for initialization
+    void Start() {
+    }
+
+    void FixedUpdate() {
+        velocity += Physics.gravity * Time.deltaTime;
+
+        if (characterController.isGrounded) {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, -transform.up, out hit)) {
+                Vector3 projectedVelocity = Vector3.ProjectOnPlane(velocity, hit.normal);
+                velocity = projectedVelocity;
+            }
+        }
+    }
+
+    public void AddVelocity(Vector3 velocity) {
+        this.velocity += velocity;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        characterController.Move(velocity * Time.deltaTime);
+    }
+
+    void OnDrawGizmos() {
+        Gizmos.DrawRay(transform.position, velocity);
+    }
+
+    public void MovementInput(Vector2 input) {
+        this.input = input; 
+    }
+}
+
+[Serializable]
+public struct GroudAttributes {
+    public float friction; // value between 0 (no friction) and 1 (no velocity loss)
+}
---
+++ UnityProject/Assets/Jetpack.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterMovement))]
+public class Jetpack : MonoBehaviour, IJetpack {
+    public float strength = 0.25f;
+    public float _maxFuel = 100;
+    public float fuelConsumptionRate = 10;
+
+    private float _fuel;
+
+    private CharacterMovement slideMovement;
+
+    private bool thrustOn;
+
+
+    void Awake() {
+        slideMovement = GetComponent<CharacterMovement>();
+    }
+
+    private void Start() {
+        _fuel = _maxFuel;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        if(thrustOn && _fuel > 0) {
+            slideMovement.AddVelocity(Vector3.up * strength);
+            _fuel = Mathf.Max(_fuel - fuelConsumptionRate * Time.fixedDeltaTime, 0);
+        }
+    }
+
+    public void Thrust(bool enable) {
+        thrustOn = enable;
+    }
+
+
+    public float maxFuel {
+        get {
+            return maxFuel;
+        }
+    }
+
+    public float fuel {
+        get {
+            return _fuel;
+        }
+        set {
+            _fuel = Mathf.Clamp(value, 0, maxFuel);
+        }
+    }
+}
