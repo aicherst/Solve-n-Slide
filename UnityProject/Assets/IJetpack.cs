730fe788b7c18193b529904de0bc13afcbf52bec
Markus <Markus.Ansorge@gmx.de>
2017-05-11 16:58:44 +0200

    Added jetpack interface

---
+++ UnityProject/Assets/IJetpack.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IJetpack {
+    void Thrust(bool enable);
+
+    float maxFuel {
+        get;
+    }
+
+    float fuel {
+        get;
+        set;
+    }
+}
