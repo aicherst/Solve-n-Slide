cb30690cc24b391ebc35ccecb93064f38f825ced
Stefan Aicher <stefanaicher1@gmx.de>
2017-05-21 23:27:04 +0200

    Implement parts of phase switch implement switch (start in Manipulation-Phase; press enter to switch back and forth) implement changes to UI implement position switch (Action-Phase->startPosition; Manipulation-Phase->lastManipulationPosition) disable sliding and jetpack movement during Manipulation-Phase

--- UnityProject/Assets/Character.cs
+++ UnityProject/Assets/Character.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class Character : MonoBehaviour {
     public int health = 100, maxHealth = 100;
-    public int charges = 3, maxCharges = 5;
+    public int charges = 5, maxCharges = 5;
     IJetpack jetpack;
     ICharacterMovement characterMovement;
+    public enum Phase {
+        MANIPULATION_PHASE, ACTION_PHASE
+    }
+    Phase currentPhase = Phase.MANIPULATION_PHASE;
+
+    Vector3 startPosition;
+    Vector3 manipulationPosition;
 
     void Awake() {
         jetpack = GetComponent<IJetpack>();
@@ -15,10 +22,36 @@ public class Character : MonoBehaviour {
 
     // Use this for initialization
     void Start() {
+        startPosition = transform.position;
+        ((Jetpack)jetpack).enabled = false;
+        ((CharacterMovement)characterMovement).enabled = false;
     }
 
     // Update is called once per frame
     void Update() {
+        if(Input.GetButtonDown("PhaseSwitch")) {
+            if (currentPhase == Phase.MANIPULATION_PHASE) {
+                currentPhase = Phase.ACTION_PHASE;
+
+                ((Jetpack)jetpack).enabled = true;
+                ((CharacterMovement)characterMovement).enabled = true;
+
+                transform.position = startPosition;
+                manipulationPosition = transform.position;
+
+                health = maxHealth;
+                charges = maxCharges;
+                //TODO: reset jetpack fuel
+            } else {
+                currentPhase = Phase.MANIPULATION_PHASE;
+
+                ((Jetpack)jetpack).enabled = false;
+                ((CharacterMovement)characterMovement).enabled = false;
+
+                transform.position = manipulationPosition;
+            }
+        }
+
         jetpack.Thrust(Input.GetButton("Jetpack"));
         Vector2 movementAxis = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         characterMovement.MovementInput(movementAxis);
@@ -32,4 +65,6 @@ public class Character : MonoBehaviour {
 
     public int getCharges() { return charges; }
     public int getMaxCharges() { return maxCharges; }
+
+    public Phase getCurrentPhase() { return currentPhase; }
 }
--- UnityProject/Assets/UIController.cs
+++ UnityProject/Assets/UIController.cs
@@ -5,8 +5,11 @@ using UnityEngine.UI;
 
 public class UIController : MonoBehaviour {
     public Text healthText;
+    public Image healthBackground;
     public Text fuelText;
+    public Image fuelBackground;
     public Text chargesText;
+    public Image chargesBackground;
     public Character character;
 
     // Use this for initialization
@@ -15,8 +18,16 @@ public class UIController : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+        healthText.enabled = character.getCurrentPhase() == Character.Phase.ACTION_PHASE;
+        healthBackground.enabled = character.getCurrentPhase() == Character.Phase.ACTION_PHASE;
         healthText.text = "Test Health: " + makeThreeChars(character.getHealth()) + "/" + makeThreeChars(character.getMaxHealth());
+
+        fuelText.enabled = character.getCurrentPhase() == Character.Phase.ACTION_PHASE;
+        fuelBackground.enabled = character.getCurrentPhase() == Character.Phase.ACTION_PHASE;
         fuelText.text = "Test Fuel: " + makeThreeChars(Mathf.RoundToInt(character.getFuel())) + "/" + makeThreeChars(Mathf.RoundToInt(character.getMaxFuel()));
+
+        chargesText.enabled = character.getCurrentPhase() == Character.Phase.MANIPULATION_PHASE;
+        chargesBackground.enabled = character.getCurrentPhase() == Character.Phase.MANIPULATION_PHASE;
         chargesText.text = "Test Charges: " + character.getCharges() + "/" + character.getMaxCharges();
     }
 
