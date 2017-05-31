69bd1ad3d7a04dc17fc81fdbb79045bcfae05801
Kevin Sawischa <Kevin.Sawischa@gmx.de>
2017-05-31 16:36:22 +0200

    Manipulation Camera and Charge use

---
+++ UnityProject/Assets/Scripts/ManipulationCamera.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ManipulationCamera : MonoBehaviour {
+	public float sensitivityX = 15F;
+	public float sensitivityY = 15F;
+
+	float rotationY = 0F;
+	public Camera egoCamera;
+
+	void Update () {
+		float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+
+		rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+		rotationY = Mathf.Clamp(rotationY, -90, 90);
+
+		egoCamera.transform.localEulerAngles = new Vector3(-rotationY, 0, 0);
+		transform.localEulerAngles = new Vector3(0, rotationX, 0);
+
+		transform.Translate(Vector3.right * Input.GetAxis("Horizontal"));
+		transform.Translate(Vector3.forward * Input.GetAxis("Vertical"));
+	}
+
+	void Start () {
+	}
+}
--- UnityProject/Assets/Scripts/Player.cs
+++ UnityProject/Assets/Scripts/Player.cs
@@ -16,11 +16,32 @@ public class Player : MonoBehaviour {
     private GameObject actionCharacterInstance;
     public GameObject manipulationCharacterPrefab;
     private GameObject manipulationCharacterInstance;
+	
+	//for the terrain
+	private static Terrain levelTerrain;
+	private int alphaMapWidth;
+	private int alphaMapHeight;
+	private float[,] heightMapBackup;
+	private float[,,] alphaMapBackup;
 
     // Use this for initialization
     void Start () {
         startPosition = transform.position;
         manipulationCharacterInstance = Instantiate(manipulationCharacterPrefab, startPosition, Quaternion.identity);
+		
+		//for terrain
+		levelTerrain = Terrain.activeTerrain;
+
+		alphaMapWidth = levelTerrain.terrainData.alphamapWidth;
+		alphaMapHeight = levelTerrain.terrainData.alphamapHeight;
+
+		heightMapBackup = levelTerrain.terrainData.GetHeights(0, 0, levelTerrain.terrainData.heightmapWidth, levelTerrain.terrainData.heightmapHeight);
+		alphaMapBackup = levelTerrain.terrainData.GetAlphamaps(0, 0, alphaMapWidth, alphaMapHeight);
+	}
+	
+	void OnApplicationQuit () {
+		levelTerrain.terrainData.SetHeights(0, 0, heightMapBackup);
+		levelTerrain.terrainData.SetAlphamaps(0, 0, alphaMapBackup);
 	}
 	
 	// Update is called once per frame
@@ -51,4 +72,5 @@ public class Player : MonoBehaviour {
     public Phase getCurrentPhase() { return currentPhase; }
     public ActionCharacter getActionCharacter() { return actionCharacterInstance.GetComponent<ActionCharacter>(); }
     public ManipulationCharacter getManipulationCharacter() { return manipulationCharacterInstance.GetComponent<ManipulationCharacter>(); }
+	public static Terrain getLevelTerrain () {	return levelTerrain; }
 }
