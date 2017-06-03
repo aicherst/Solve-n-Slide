e52564868ba11039e883b01107b057160438468b
Kevin Sawischa <Kevin.Sawischa@gmx.de>
2017-06-03 09:28:42 +0200

    The perfect hill
    look at it, its beautiful (Terrain manipulation smooth now)

--- UnityProject/Assets/Scripts/ManipulationCharacter.cs
+++ UnityProject/Assets/Scripts/ManipulationCharacter.cs
@@ -18,34 +18,34 @@ public class ManipulationCharacter : MonoBehaviour {
 	}
 
 	void Update () {
+		//raise terrain
 		if (Input.GetMouseButtonDown(0) && charges >= 1) {
 			charges--;
 			RaycastHit hit;
-			Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f,0.5f,0f));//Camera.main.ScreenPointToRay(Input.mousePosition);
+			Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f,0.5f,0f));
 			if (Physics.Raycast(ray, out hit)) {
-				manipulateTerrainArea(hit.point, 50, 50, true); 
+				manipulateTerrainArea(hit.point, 25, true); 
 			}
 		}
+		//lower terrain
 		if (Input.GetMouseButtonDown(1) && charges >= 1) {
 			charges--;
 			RaycastHit hit;
-			Ray ray = Camera.main.ViewportPointToRay (new Vector3(0.5f,0.5f,0f));//Camera.main.ScreenPointToRay(Input.mousePosition);
+			Ray ray = Camera.main.ViewportPointToRay (new Vector3(0.5f,0.5f,0f));
 			if (Physics.Raycast(ray, out hit)) {
-				manipulateTerrainArea(hit.point, 50, 50, false);
+				manipulateTerrainArea(hit.point, 25, false);
 			}
 		}
 	}
 
 
-	private void manipulateTerrainArea (Vector3 point, int lenx, int lenz, bool raise) {
+	private void manipulateTerrainArea (Vector3 point, int r, bool raise) {
 		int areax;
 		int areaz;
 		int terX = (int)((point.x / levelTerrain.terrainData.size.x) * xResolution);
 		int terZ = (int)((point.z / levelTerrain.terrainData.size.z) * zResolution);
-		lenx += 0;
-		lenz += 0;
-		terX -= (lenx / 2);
-		terZ -= (lenz / 2);
+		terX -= r;
+		terZ -= r;
 		if (terX < 0)
 			terX = 0;
 		if (terX > xResolution)
@@ -54,21 +54,22 @@ public class ManipulationCharacter : MonoBehaviour {
 			terZ = 0;
 		if (terZ > zResolution)
 			terZ = zResolution;
-		float[,] heights = levelTerrain.terrainData.GetHeights(terX, terZ, lenx, lenz);
-		for (areax = 0; areax < lenx; areax++) {
-			for (areaz = 0; areaz < lenz; areaz++) {
-				if ((areax > -1) && (areaz > -1) && (areax < xResolution) && (areaz < zResolution)) {
-					float intensity = 25 + Mathf.Max(Mathf.Abs(areax - 25), Mathf.Abs(areaz - 25));
+		float[,] heights = levelTerrain.terrainData.GetHeights(terX, terZ, (r*2), (r*2));
 
+		for (areax = -r; areax < r; areax++) {
+			for (areaz = -r; areaz < r; areaz++) {
+				int y = (r * r) - ((areax * areax) + (areaz * areaz));
+				if (y > 0 && areax < xResolution && areaz < zResolution) {
 					if (raise == true) {
-						heights[areax, areaz] += (12.5f / levelTerrain.terrainData.size.y) * Mathf.Pow(0.9f, intensity);
+						heights[areax + r, areaz + r] += (float)y / (150f * levelTerrain.terrainData.size.y);
 					}
 					else {
-						heights[areax, areaz] -= (12.5f / levelTerrain.terrainData.size.y) * Mathf.Pow(0.9f, intensity);
+						heights[areax + r, areaz + r] -= (float)y / (150f * levelTerrain.terrainData.size.y);
 					}
 				}
 			}
 		}
+
 		levelTerrain.terrainData.SetHeights(terX, terZ, heights);
 	}
 
