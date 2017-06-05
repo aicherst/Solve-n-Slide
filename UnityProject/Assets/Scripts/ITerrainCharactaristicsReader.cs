7d499bd8e03ae8ebaea61af18f79fa5288b2d6ee
Kevin Sawischa <Kevin.Sawischa@gmx.de>
2017-06-05 21:06:43 +0200

    Part of TerrainCharacteristicsReader

---
+++ UnityProject/Assets/Scripts/ITerrainCharactaristicsReader.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public abstract class ITerrainCharactaristicsReader {
+
+	public struct TerrainCharacteristics {
+		public float friction;
+
+		public TerrainCharacteristics (float friction) {
+			this.friction = friction;
+		}
+	}
+
+	public abstract TerrainCharacteristics GetTerrainCharacteristics (Vector3 position);
+}
---
+++ UnityProject/Assets/Scripts/TerrainCharactaristicsReader.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class TerrainCharactaristicsReader : ITerrainCharactaristicsReader {
+	
+	private static Terrain terrain;
+
+	void Start () {
+		terrain = ManipulationCharacter.getLevelTerrain();
+	}
+
+	private static float[] GetTextureMix (Vector3 position) {
+		int mapX = (int)(((position.x - terrain.transform.position.x) / terrain.terrainData.size.x) * terrain.terrainData.alphamapWidth);
+		int mapZ = (int)(((position.z - terrain.transform.position.z) / terrain.terrainData.size.z) * terrain.terrainData.alphamapHeight);
+         
+		float[,,] splatmapData = terrain.terrainData.GetAlphamaps(mapX, mapZ, 1, 1);
+        
+		float[] cellMix = new float[ splatmapData.GetUpperBound(2) + 1 ];
+         
+		for (int n = 0; n < cellMix.Length; n++) {
+			cellMix[n] = splatmapData[0, 0, n];
+		}
+		return cellMix;
+	}
+
+	public static int GetMainTexture (Vector3 position) {
+		float[] mix = GetTextureMix(position);
+         
+		float maxMix = 0;
+		int maxIndex = 0;
+        
+		for (int n = 0; n < mix.Length; n++) {
+			if (mix[n] > maxMix) {
+				maxIndex = n;
+				maxMix = mix[n];
+			}
+		}
+		return maxIndex;
+	}
+
+	public override TerrainCharacteristics GetTerrainCharacteristics (Vector3 position) {
+		return new TerrainCharacteristics();
+	}
+}
