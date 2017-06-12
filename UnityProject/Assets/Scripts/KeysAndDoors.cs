1d5ab4965c001eb8080ea3a55df9ff819a964eaf
Kevin Sawischa <Kevin.Sawischa@gmx.de>
2017-06-12 17:12:06 +0200

    Simple Key/Door

---
+++ UnityProject/Assets/Scripts/KeysAndDoors.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeysAndDoors : MonoBehaviour {
+
+	public static int keys = 0;
+	
+	void OnTriggerEnter(Collider other) {
+		if(other.tag == "Key") {
+			keys++;
+			Destroy(other.gameObject);
+		}
+		else if (other.tag == "Door" && keys >= 1) {
+			keys--;
+			Destroy(other.transform.parent.gameObject);
+		}
+	}
+}
