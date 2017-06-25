1c3fc54b62bd6866258f45a30f5cd7ba59382bfc
Stefan Aicher <stefanaicher1@gmx.de>
2017-06-25 23:58:20 +0200

    Finish main menu

--- UnityProject/Assets/Scripts/MainMenuController.cs
+++ UnityProject/Assets/Scripts/MainMenuController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour {
-    public GameObject optionsCube, exitCube, level1Cube, level2Cube, level3Cube, level4Cube;
+    public GameObject optionsCube, exitCube, level1Cube, level2Cube, level3Cube, level4Cube, level5Cube;
 
 	// Use this for initialization
 	void Start () {
@@ -14,6 +14,14 @@ public class MainMenuController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        optionsCube.GetComponent<MeshRenderer>().material.color = new Color(255, 255, 255, 255);
+        exitCube.GetComponent<MeshRenderer>().material.color = new Color(255, 255, 255, 255);
+        level1Cube.GetComponent<MeshRenderer>().material.color = new Color(255, 255, 255, 255);
+        level2Cube.GetComponent<MeshRenderer>().material.color = new Color(255, 255, 255, 255);
+        level3Cube.GetComponent<MeshRenderer>().material.color = new Color(255, 255, 255, 255);
+        level4Cube.GetComponent<MeshRenderer>().material.color = new Color(255, 255, 255, 255);
+        level5Cube.GetComponent<MeshRenderer>().material.color = new Color(255, 255, 255, 255);
+
         bool mousePressed = Input.GetMouseButtonDown(0);
         RaycastHit hit;
         Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
@@ -22,48 +30,42 @@ public class MainMenuController : MonoBehaviour {
                 if (mousePressed)
                     Debug.Log("Options");
                 optionsCube.GetComponent<MeshRenderer>().material.color = new Color(255, 0, 0, 255);
-            } else {
-                optionsCube.GetComponent<MeshRenderer>().material.color = new Color(255, 255, 255, 255);
             }
 
             if (hit.collider.gameObject == exitCube) {
                 if (mousePressed)
                     Application.Quit();
                 exitCube.GetComponent<MeshRenderer>().material.color = new Color(255, 0, 0, 255);
-            } else {
-                exitCube.GetComponent<MeshRenderer>().material.color = new Color(255, 255, 255, 255);
             }
 
             if (hit.collider.gameObject == level1Cube) {
                 if (mousePressed)
                     SceneManager.LoadScene(1);
                 level1Cube.GetComponent<MeshRenderer>().material.color = new Color(255, 0, 0, 255);
-            } else {
-                level1Cube.GetComponent<MeshRenderer>().material.color = new Color(255, 255, 255, 255);
             }
 
             if (hit.collider.gameObject == level2Cube) {
                 if (mousePressed)
                     SceneManager.LoadScene(2);
                 level2Cube.GetComponent<MeshRenderer>().material.color = new Color(255, 0, 0, 255);
-            } else {
-                level2Cube.GetComponent<MeshRenderer>().material.color = new Color(255, 255, 255, 255);
             }
 
             if (hit.collider.gameObject == level3Cube) {
                 if (mousePressed)
-                    Debug.Log("Level3");
+                    SceneManager.LoadScene(3);
                 level3Cube.GetComponent<MeshRenderer>().material.color = new Color(255, 0, 0, 255);
-            } else {
-                level3Cube.GetComponent<MeshRenderer>().material.color = new Color(255, 255, 255, 255);
             }
 
             if (hit.collider.gameObject == level4Cube) {
                 if (mousePressed)
-                    Debug.Log("Level4");
+                    SceneManager.LoadScene(4);
                 level4Cube.GetComponent<MeshRenderer>().material.color = new Color(255, 0, 0, 255);
-            } else {
-                level4Cube.GetComponent<MeshRenderer>().material.color = new Color(255, 255, 255, 255);
+            }
+
+            if (hit.collider.gameObject == level5Cube) {
+                if (mousePressed)
+                    SceneManager.LoadScene(5);
+                level5Cube.GetComponent<MeshRenderer>().material.color = new Color(255, 0, 0, 255);
             }
         }
     }
