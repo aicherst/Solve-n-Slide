2ed7c3e34a15f7e34d7f46ece4a8d7e332aca111
Markus <Markus.Ansorge@gmx.de>
2017-07-17 15:01:00 +0200

    Added Drone Tutorial

---
+++ UnityProject/Assets/DisableChildrenDuringActionPhase.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableChildrenDuringActionPhase : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+        GameStateManager.instance.gamePhase.AddListener(OnGamePhaseChange);
+    }
+
+    public void OnGamePhaseChange(ReadOnlyProperty<GamePhase> changedProperty, GamePhase newData, GamePhase oldData) {
+        switch (newData) {
+            case GamePhase.Manipulation:
+                EnableChildren(true);
+                break;
+            default:
+                EnableChildren(false);
+                break;
+        }
+    }
+
+    private void EnableChildren(bool enable) {
+        foreach(Transform child in transform) {
+            child.gameObject.SetActive(enable);
+        }
+    }
+
+    private void OnDestroy() {
+        GameStateManager.instance.gamePhase.RemoveListener(OnGamePhaseChange);
+    }
+}
